afc304c15d95541357b8d1a6362adb41b0024d4e
Irfan Hardiyanto <dev.hardiyanto@gmail.com>
2025-08-21 18:53:19 +0700

    main: initial update for breeze vue+inertia

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,7 +1,20 @@
+import type { LucideIcon } from 'lucide-vue-next';
 import type { Config } from 'ziggy-js';
 
 export interface Auth {
     user: User;
+}
+
+export interface BreadcrumbItem {
+    title: string;
+    href: string;
+}
+
+export interface NavItem {
+    title: string;
+    href: string;
+    icon?: LucideIcon;
+    isActive?: boolean;
 }
 
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
@@ -9,6 +22,7 @@ export type AppPageProps<T extends Record<string, unknown> = Record<string, unkn
     quote: { message: string; author: string };
     auth: Auth;
     ziggy: Config & { location: string };
+    sidebarOpen: boolean;
 };
 
 export interface User {
@@ -20,3 +34,5 @@ export interface User {
     created_at: string;
     updated_at: string;
 }
+
+export type BreadcrumbItemType = BreadcrumbItem;
